9d3866bcd70fd9d38061c65569972d9d72b3395b
gigscraig <pauldoylecraig@gmail.com>
2016-02-23 22:01:58 +0000

    results reversed

---
+++ js/playerName.js
@@ -0,0 +1,12 @@
+//$(document).ready(function () {
+//
+//
+//    var str = document.getElementsByClassName("player").innerHTML;
+//    var res = str.replace("L Ellis", "P Craig");
+//    document.getElementsByClassName("player").innerHTML = res;
+//
+//
+//
+//
+//
+//});
